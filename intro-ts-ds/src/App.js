a01f250083e5d89c62084b53008cebded4101385
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-04-04 17:03:56 -0500

    Completed Profile component

--- intro-ts-ds/src/App.js
+++ intro-ts-ds/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import BravoComponent from './components/BravoComponent'
+import Profile from './components/Profile'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BravoComponent />
+        <Profile />
       </div>
     )
   }
