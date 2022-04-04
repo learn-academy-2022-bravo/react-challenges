14a4fbfce8fc5dfae35810f022861781a95602da
ecostrong <necoarmstrong@mac.com>
2022-04-04 16:28:33 -0700

    second commit

--- intro-neco-jack/src/App.js
+++ intro-neco-jack/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
 import Profile from './components/Profile'
+import Favorites from './components/Favorites'
 import Footer from './components/Footer'
+import Title from './components/Title'
+
 
 class App extends React.Component{
   render() {
     return(
       <>
-      <Header />
+      <Header/>
       <Profile />
+      <Favorites/>
       <Footer />
       </>
     )
---
+++ intro-neco-jack/src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Title extends React.Component {
+    render() {
+        return(
+            <title>Customized Browser Tab</title>
+        )
+    }
+}
+
+export default Title
