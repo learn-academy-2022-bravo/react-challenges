66ad3215425bb2f4ea4e9a1efa1f760ded37ce56
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-04-04 16:58:46 -0500

    Initial commit and first component made.

---
+++ intro-ts-ds/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BravoComponent from './components/BravoComponent'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BravoComponent />
+      </div>
+    )
+  }
+}
