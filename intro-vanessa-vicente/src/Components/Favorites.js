f5a34b4e4eff4c35913a78019f3ed22a05a07cf9
VicenteSaldana1 <vsaldana210@gmail.com>
2022-04-04 16:16:35 -0700

    initial commit of fitness app

---
+++ intro-vanessa-vicente/src/Components/Favorites.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  render (){
+    return (
+      <>
+      <ul>
+      <li> Do something that you enjoy! </li>
+      <li> Set realistic goals for yourself! </li>
+      <li> Prioritize protein intake while eating foods that you enjoy. </li>
+      <li> Allow yourself adequate time for rest and recovery (aim for 8 hours of sleep at night). </li>
+      <li> Focus on creating small changes and celebrate even the smallest accomplishments in your fitness journey! </li>
+      </ul>
+      </>
+    )
+  }
+}
+
+export default App
