67d1804ca49398de43d65807980add6f26f52958
cjpriedel <cjpriedel@gmail.com>
2022-04-04 15:42:05 -0700

    react app component testing

---
+++ intro-cj-van/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class Footer extends Component {
+    render(){
+        return(
+            <>
+            <h3>Bravo 2022</h3>
+            </>
+        )
+    }
+}
+
---
+++ intro-cj-van/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class Header extends Component {
+    render(){
+        return(
+            <>
+            <h1>Cj and Vans App</h1>
+            </>
+        )
+    }
+}
+
