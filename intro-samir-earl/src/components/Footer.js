a8d2fd3c6781783907c93989cb08173d875022a3
earl-nicholson <earl.r.nicholson@gmail.com>
2022-04-04 18:28:54 -0500

    first commit

---
+++ intro-samir-earl/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <>
+        <footer>samir and earl</footer>
+      </>
+    )
+  }
+}
+export default Footer
---
+++ intro-samir-earl/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Profile extends React.Component {
+  render () {
+    return (
+      <>
+        <p>
+        Samir is from New York
+        Earl is from Chicago
+        </p>
+      </>
+    )
+  }
+}
+export default Profile
