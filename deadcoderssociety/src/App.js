85923826ed34af37508861c42cc6c290683db9ed
zmprunella <zane.prunella@gmail.com>
2022-04-04 15:23:55 -0700

    New Poems

---
+++ deadcoderssociety/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react' 
+import Header from './Components/Header' 
+import Footer from './Components/Footer' 
+
+class App extends React.Component {
+  render(){
+      return (
+        <>
+        <h1> Hi Orvab!! </h1>
+        <Header/>
+        <Footer/>
+        </>
+      )
+  }
+}
+export default App 
