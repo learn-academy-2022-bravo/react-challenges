161f6c52dc8e96c8a7b160911f7b4ae838be84a6
ljkotalik <leannak14@gmail.com>
2022-04-05 14:34:45 -0700

    initial commit

---
+++ state-victor-lj/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Box from './components/Box'
+
+class App extends Component{
+
+  render(){
+    return(
+      <>
+        <h1>Challenge Color Box</h1>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ state-victor-lj/src/components/Box.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class Box extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            breaks: 0,
+            color: "rgb (123, 123, 123)"
+        }
+    }
+
+
+
+
+
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default Box
