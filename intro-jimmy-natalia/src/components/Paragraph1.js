0a75dfe00d315bdba4ca77b606b6d2db7c940dcb
Velasqj8 <jv_619@yahoo.com>
2022-04-04 22:52:07 -0700

    added different components to app

---
+++ intro-jimmy-natalia/src/components/Paragraph1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class Paragraph1 extends React.Component {
+  render() {
+    return(
+      <>
+      <p1>We both like Avocados</p1>
+      </>
+    )
+  }
+}
+export default Paragraph1
