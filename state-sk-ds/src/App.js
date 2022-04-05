227725b7ebfd255a5ee57d79abdc1fbd50fd8143
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-04-05 16:21:54 -0500

    Initial commit

--- state-sk-ds/src/App.js
+++ state-sk-ds/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// Challenge: Color Box
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// User Stories
+
+// As a user, I can see a white square on the screen
+// As a user, when I click on the square the name of a color appears in the box
+// As a user, each time I click the box I see a new color name
+// As a user, I can cycle through a list of color names, one per click - green, blue, yellow, red, purple, orange
+// As a user, when I click the box, the background changes to match the name of the color name displayed in the box
+// As a user, I can see many color boxes on the page all acting independently of one another
+// Stretch
+
+// As a user, I can see a button to add or remove color boxes
+// As a user, when I click the add button, I can add one colorbox that acts independently of the other colorboxes
+// As a user, when I click the remove button, I can remove one colorbox
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
-
-export default App;
