22173be996e8650b6fe9db920edb65d13e4a389d
ljkotalik <leannak14@gmail.com>
2022-04-05 16:28:09 -0700

    made a box

--- state-victor-lj/src/App.js
+++ state-victor-lj/src/App.js
@@ -7,6 +7,7 @@ class App extends Component{
     return(
       <>
         <h1>Challenge Color Box</h1>
+        <Box />
       </>
     )
   }
--- state-victor-lj/src/components/Box.js
+++ state-victor-lj/src/components/Box.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class Box extends Component {
-    constructor(props){
-        super(props)
-        this.state= {
-            breaks: 0,
-            color: "rgb (123, 123, 123)"
-        }
+class Box extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      color:["rgb(158, 52, 235)","rgb(158, 52, 0)"]
     }
+  }
+
+  addBreak = () => {
+    this.setState({})
+  }
 
 
-
-
-
-    render(){
-        return(
-            <>
-
-            </>
-        )
-    }
+  render(){
+    return(
+      <>
+      <div style={{ height: "250px",width: "250px", border: "5px solid blue" }}> onClick ={this.colorName}
+    </div>
+      </>
+    )
+  }
 }
 
 export default Box
