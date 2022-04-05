96de29e0bb881775173ec1e258b5c576a5ec6c68
aclassiter <a.class.wa@gmail.com>
2022-04-05 11:47:43 -0700

    commpleted challenges

---
+++ intro-andrew-amanda/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './components/Header'
+import Profile from './components/Profile'
+import Footer from './components/Footer'
+import Favorites from './components/Favorites'
+
+class App extends React.Component{
+  render() {
+    return(
+      <>
+        <Header />
+        <Profile />
+        <Footer />
+        <Favorites />
+      
+      
+      
+      </>
+    )
+  }
+}
+export default App
+
