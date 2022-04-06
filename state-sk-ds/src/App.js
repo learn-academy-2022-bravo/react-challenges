044b4d5e0d6370fae71ab372d4ee0360c41615c8
dustin-oz <schelldd@gmail.com>
2022-04-05 20:24:00 -0500

    Many boxes on the page acting independently.. no styling yet

--- state-sk-ds/src/App.js
+++ state-sk-ds/src/App.js
@@ -5,50 +5,33 @@
 // As a user, I can see a white square on the screen
 
 // As a user, when I click on the square the name of a color appears in the box
+
 // As a user, each time I click the box I see a new color name
+
 // As a user, I can cycle through a list of color names, one per click - green, blue, yellow, red, purple, orange
+
 // As a user, when I click the box, the background changes to match the name of the color name displayed in the box
+
 // As a user, I can see many color boxes on the page all acting independently of one another
 // Stretch
+
 
 // As a user, I can see a button to add or remove color boxes
 // As a user, when I click the add button, I can add one colorbox that acts independently of the other colorboxes
 // As a user, when I click the remove button, I can remove one colorbox
 
 import React, { Component } from 'react'
+import Box from './components/Box'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      colors: "",
-      backgroundColor: "white"
-    }
-  }
-
-  randomColor = () => {
-
-    let colorSelection = ["green", "blue", "yellow", "red", "purple", "orange"]
-    let randomizer = colorSelection[Math.floor(Math.random() * colorSelection.length)]
-
-    this.setState({ colors: randomizer, backgroundColor: randomizer })
-  }
-
   render() {
-
     return (
-      <div>
-        <div id="theBox" style={{
-          border: "5px solid black",
-          width: "100px",
-          height: "100px",
-          backgroundColor: this.state.backgroundColor,
-          color: 'black'
-        }} onClick={this.randomColor}>
-          {this.state.colors}
-        </div>
-        <button id="theButton" onClick={this.randomColor}>Change Color of Box</button>
-      </div>
+      <>
+        <Box />
+        <Box />
+        <Box />
+      </>
     )
   }
 }
+
---
+++ state-sk-ds/src/components/Box.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            colors: "",
+            backgroundColor: "white"
+        }
+    }
+
+    randomColor = () => {
+
+        let colorSelection = ["green", "blue", "yellow", "red", "purple", "orange"]
+        let randomizer = colorSelection[Math.floor(Math.random() * colorSelection.length)]
+
+        this.setState({ colors: randomizer, backgroundColor: randomizer })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div id="theBox" style={{
+                    border: "5px solid black",
+                    width: "100px",
+                    height: "100px",
+                    backgroundColor: this.state.backgroundColor,
+                    color: 'black'
+                }} onClick={this.randomColor}>
+                    {this.state.colors}
+                </div>
+                <button id="theButton" onClick={this.randomColor}>Change Color of Box</button>
+            </div>
+        )
+    }
+}
