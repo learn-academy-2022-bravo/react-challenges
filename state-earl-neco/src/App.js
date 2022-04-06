b21057de0d62a153e683ae7c6e353243104c077b
earl-nicholson <earl.r.nicholson@gmail.com>
2022-04-06 00:39:42 -0500

    color change

--- state-earl-neco/src/App.js
+++ state-earl-neco/src/App.js
@@ -4,14 +4,24 @@ class App extends Component {
     constructor(props){
       super(props)
       this.state ={
-        color:["red"]
+        color:["white", "red", "blue", "purple"]
+
+
+      }
+    }
+    changeColor = () => {
+      for (let i=0; i < this.state.color.length; i++){
+        let nextColor=0
+        this.setState({color: this.state.color[nextColor+i]})
       }
     }
   render() {
     return (
       <>
       <h1>Color Box</h1>
-      <div style={{ height:"100px", width:"100px",backgroundColor: this.state.color}}></div>
+      <div style={{ height:"100px", width:"100px", border: "2px solid black",backgroundColor: this.state.color}}
+      onClick= {this.changeColor}> </div>
+
     </>
     )
   }
