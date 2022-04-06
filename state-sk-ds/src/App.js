0a23f6919c57c5211f31910cf8e4f1aec7ee33ff
dustin-oz <schelldd@gmail.com>
2022-04-05 20:07:58 -0500

    Clicking on square now changes the color text randomly

--- state-sk-ds/src/App.js
+++ state-sk-ds/src/App.js
@@ -3,6 +3,7 @@
 // User Stories
 
 // As a user, I can see a white square on the screen
+
 // As a user, when I click on the square the name of a color appears in the box
 // As a user, each time I click the box I see a new color name
 // As a user, I can cycle through a list of color names, one per click - green, blue, yellow, red, purple, orange
@@ -42,7 +43,7 @@ export default class App extends Component {
           height: "100px",
           backgroundColor: "white",
           color: 'black'
-        }}>
+        }} onClick={this.randomColor}>
           {this.state.colors}
         </div>
         <button id="theButton" onClick={this.randomColor}>Name of Color</button>
