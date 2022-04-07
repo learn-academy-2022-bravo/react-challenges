6524e0b02b7ed5af38aca3eeb31a1ca3e47d7d5b
ecostrong <necoarmstrong@mac.com>
2022-04-07 11:55:53 -0700

    Dice Roller App planning

---
+++ props-neco-vicente/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+// Using a well thought out state tree and nested component structure, construct an application that rolls a die and keeps track of the numbers rolled. Here is a wireframe to help you start planning your application:
+// //As a user, I can see an application called Dice Roller
+// // As a developer, I can create a React application with App.js as my stateful component
+//As a developer, I can create two child components that will accept props from App.js
+// As a user, I can click a box and see the outcome of my current "roll"
+// As a developer, I can pass a method from App.js to my dice component to display a number between 1 and 6
+// As a user, I can see my roll logged
+// As a developer, I can pass the value of the roll to a log component
+// As a user, I can see the roll log continue to grow as I roll the dice
+
+//1.Do last? App.css: wireframe image on syllubus for formating example- Use Vicente's box code
+//2. Create State Tree
+
+
+
+
+
+// 3. Create nested component structure
+// 4. Parent: is App.js This is an application that rolls a die
+// 5. Child: A component that keeps track of the numbers rolled
+// 6. Put the name of app "Dice Roller" so that it appears in web browser put code in App.js
+// 7. In App.js create a react app, as the state
+// 8. Make 2 child components that will accept props from App.js
+//    Component Child 1-"DiceGambler"
+//    Component Child 2-"DiceTracker"
+// 9. Add a button with an onClick, that shows outcome of current "roller"
+// 10. Add to existing method in App.js, pass this from parent to child component Child 1
+// 11. Display roll logged in Web browser
+// 12. In the Component Child 2-"DiceTracker", display log the previous rolls
+// 13. In the Component Child 2-"DiceTracker", display log that continues to track rolls each time the dice is rolled.
+
+
+
+
+
+
+
+
+
+
+
+export default App;
