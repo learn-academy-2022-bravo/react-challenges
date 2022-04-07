34d6f5116053f0a57d0319ee7c46eec971bf6fa8
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-04-07 14:31:40 -0500

    Initial Commit and initial blocking out of needed files and some elements - just before doing pseudo code

---
+++ props-cj-ds/src/App.js
@@ -0,0 +1,23 @@
+// import './App.css';
+
+// import React, { Component } from 'react'
+// import DiceRestartButton from './components/DiceRestartButton';
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       diceRollImages: [side1, side2, side3, side4, side5, side6],
+//       currentRollDice: diceHero,
+//       diceRollHistory: []
+//     }
+//   }
+//   render() {
+//     return (
+//       <>
+//         <DiceRestartButton />
+//       </>
+//     )
+//   }
+// }
+
---
+++ props-cj-ds/src/components/DiceRestartButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class DiceRestartButton extends Component {
+    render() {
+        return (
+            <div>
+
+                <button>Restart Button</button>
+            </div>
+        )
+    }
+}
