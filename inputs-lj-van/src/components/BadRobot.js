7e4de8109bc8a12052da93ba579010ab5b142f96
ljkotalik <leannak14@gmail.com>
2022-04-08 13:13:24 -0700

    all 3 bots working

--- inputs-lj-van/src/components/BadRobot.js
+++ inputs-lj-van/src/components/BadRobot.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react'
 
 class BadRobot extends React.Component {
 
-weAreYelling = (input) => {
-  return input.toUpperCase()
-}
+  blaBla = (string) => {
+    let splitArray = string.split("")
+      
+  let badRobotArray = []
+  for(let i = 0; i<splitArray.length; i++) {
+    badRobotArray.push("B","L","A")
+  }
+    return badRobotArray 
+  }
+
 
   render() {
     return (
       <>
       <h3> Bad Robot </h3>
-      <p> I hear you saying {this.props.userInput} is that correct? </p>
+      <p> I hear you saying {this.blaBla(this.props.userInput)} is that correct? </p>
       </>
     );
   }
