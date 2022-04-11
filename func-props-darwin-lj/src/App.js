9a9499d6ce5be0180fef1a0f7f6a0bb516dde8cb
ljkotalik <leannak14@gmail.com>
2022-04-11 13:05:24 -0700

    initial commit

---
+++ func-props-darwin-lj/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Order from './components/Order';
+import Price from './components/Price';
+
+class App extends Component {
+  constructor(props)
+  super(props)
+  this.state = {
+    foodItems: [
+    //   {item: "burger", price: "6.00"},
+    //   item: "fries", price: "3.00",
+    // ],
+    selectedItems: []
+  }
+
+
+
+
+
+
+
+
+
+
+
+  render() {
+    return (
+      <>
+        <h1>The Darwin & LJ Experience</h1>
+
+      </>
+    );
+  }
+}
+
+export default App;
+
+
