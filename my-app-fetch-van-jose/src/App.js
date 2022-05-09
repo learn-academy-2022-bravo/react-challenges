559b141966d5fc4af06d9ebceb1d46c8dabc63c0
Van Dexter <vanguardstellar@gmail.com>
2022-05-09 14:31:56 -0700

    mars rover pics

---
+++ my-app-fetch-van-jose/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+// import mockNasaData from './nasaMock.js';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nasaData: "",
+      marsPhoto: null
+    }
+  }
+
+componentDidMount(){
+  const apiKey = process.env.REACT_APP_MY_NASA_API_KEY
+  fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(payload => this.setState({nasaData: payload.photos}))
+  .catch(error => console.log(error))
+}
+
+  getPic = () => {
+    let randomPhoto = Math.floor(Math.random() * this.state.nasaData.length)
+    this.setState({marsPhoto: this.state.nasaData[randomPhoto].img_src})
+  }
+render (){
+  return(
+  <>
+  <h1> Mars Rover pics </h1>
+    <button onClick={this.getPic}>Get Mars Pic </button>
+    <br />
+    <br />
+    {this.state.marsPhoto &&
+    <img src={this.state.marsPhoto} alt= "mars rover" height="200px" />
+  }
+  </>
+)
+}
+}
+  export default App
